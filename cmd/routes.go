bf12af3e4e2ee10d4525937ba72d169dbb71f59d
Nicolai Negru <negru.n@gmail.com>
2024-03-31 12:06:35 +0100

    add static files

--- cmd/routes.go
+++ cmd/routes.go
@@ -16,8 +16,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(SessionLoad)
 
 	mux.Get("/", handlers.Repo.Home)
-	mux.Get("/home", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	mux.Handle("/static/*", http.StripPrefix("/static", http.FileServer(http.Dir("./static/"))))
 	return mux
 }
